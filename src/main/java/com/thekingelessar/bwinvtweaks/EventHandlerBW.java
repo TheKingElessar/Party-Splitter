d99e8e717f0ff53ad2bbbc71c4ded3b585b8db5a
TheKingElessar <wasabimath@gmail.com>
2020-03-30 00:11:57 -0600

    Added handler; it passes functionality to Loadout class

--- src/main/java/com/thekingelessar/bwinvtweaks/EventHandlerBW.java
+++ src/main/java/com/thekingelessar/bwinvtweaks/EventHandlerBW.java
@@ -1,12 +1,16 @@
 package com.thekingelessar.bwinvtweaks;
 
-import net.minecraftforge.event.entity.player.PlayerPickupXpEvent;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.InventoryPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
-public class EventHandler
+public class EventHandlerBW
 {
     @SubscribeEvent
-    public void onPlayerPickupXP(PlayerPickupXpEvent e) {
-        e.entityPlayer.setHealth(0);
+    public void onInventoryChange(PlayerInteractEvent e)
+    {
+        Loadout loadout = new Loadout(e);
     }
 }
