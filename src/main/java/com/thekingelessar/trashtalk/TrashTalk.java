1ba899e58c7c22f0e80cb7b8e1d632844e636810
TheKingElessar <wasabimath@gmail.com>
2020-04-07 20:00:30 -0600

    Updated to trash talk

---
+++ src/main/java/com/thekingelessar/trashtalk/TrashTalk.java
@@ -0,0 +1,11 @@
+package com.thekingelessar.trashtalk;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod (modid = TrashTalk.MODID, version = TrashTalk.VERSION)
+public class TrashTalk
+{
+    public static final String MODID = "trashtalk";
+    public static final String VERSION = "0.1";
+    
+}
