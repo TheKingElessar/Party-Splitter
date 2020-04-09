81f9dbe3cf6ec43389f384ebe8e35af2f1c203b6
TheKingElessar <wasabimath@gmail.com>
2020-04-08 21:26:09 -0600

    Added hotkeys for reloading and toggling /shout.

--- src/main/java/com/thekingelessar/trashtalk/TrashTalk.java
+++ src/main/java/com/thekingelessar/trashtalk/TrashTalk.java
@@ -6,32 +6,35 @@ import net.minecraftforge.fml.client.registry.ClientRegistry;
 import net.minecraftforge.fml.common.FMLCommonHandler;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.fml.common.gameevent.InputEvent;
 import org.lwjgl.input.Keyboard;
 
 @Mod (modid = TrashTalk.MODID, version = TrashTalk.VERSION)
 public class TrashTalk
 {
     public static final String MODID = "trashtalk";
-    public static final String VERSION = "0.1";
+    public static final String VERSION = "0.2";
     
     public static KeyBinding[] keyBindings;
     public static ChatMessageHandler chatMessageHandler = new ChatMessageHandler();
     
     @Mod.EventHandler
-    public void init(FMLInitializationEvent e) {
+    public void init(FMLInitializationEvent e)
+    {
         MinecraftForge.EVENT_BUS.register(this);
         FMLCommonHandler.instance().bus().register(this);
-    
-        keyBindings = new KeyBinding[1];
-    
+        
+        keyBindings = new KeyBinding[3];
+        
         // instantiate the key bindings
-        keyBindings[0] = new KeyBinding("key.sendtrashtalk", Keyboard.KEY_U, "key.categories.TrashTalk");
-    
+        keyBindings[0] = new KeyBinding("key.sendtrashtalk", Keyboard.KEY_R, "key.categories.TrashTalk"); // todo: make these look nicer in the options
+        keyBindings[1] = new KeyBinding("key.reloadtrashtalk", Keyboard.KEY_P, "key.categories.TrashTalk");
+        keyBindings[2] = new KeyBinding("key.toggleuseshout", Keyboard.KEY_T, "key.categories.TrashTalk");
+        
         ClientRegistry.registerKeyBinding(keyBindings[0]);
-    
+        ClientRegistry.registerKeyBinding(keyBindings[1]);
+        ClientRegistry.registerKeyBinding(keyBindings[2]);
+        
         MinecraftForge.EVENT_BUS.register(new TTEventHandler());
-    
+        
     }
 }
