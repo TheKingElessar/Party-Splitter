bc13484d732168f1e137c8a4546104ad60f65c96
TheKingElessar <wasabimath@gmail.com>
2020-05-05 19:07:09 -0600

    Updated version number

--- src/main/java/com/thekingelessar/trashtalk/TrashTalk.java
+++ src/main/java/com/thekingelessar/trashtalk/TrashTalk.java
@@ -12,7 +12,7 @@ import org.lwjgl.input.Keyboard;
 public class TrashTalk
 {
     public static final String MODID = "trashtalk";
-    public static final String VERSION = "1.3";
+    public static final String VERSION = "1.3.1";
     
     public static KeyBinding[] keyBindings;
     public static ChatMessageHandler chatMessageHandler = new ChatMessageHandler();
